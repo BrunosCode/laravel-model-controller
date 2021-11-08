64ad0d3e616a91e52c2e70d61389992480254993
brunEdo <edobruno.m@gmail.com>
2021-11-08 17:43:50 +0100

    a bit of style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset("css/app.css") }}">
   <title>Laravel Movies</title>
 </head>
 <body>
-  @foreach ($movies as $movie)
-    <div class="movie">
-      <h2 class="movie__title">{{ $movie["title"] }}</h2>
-      <p class="movie__description">Original Title: {{ $movie["original_title"] }}</p>
-      <p class="movie__description">Nationality: {{ $movie["nationality"] }}</p>
-      <p class="movie__description">Vote: {{ $movie["vote"] }}</p>
-    </div>
-  @endforeach
+  <main class="main grid--3col">
+    @foreach ($movies as $movie)
+      <div class="movie text--center">
+        <h2 class="movie__title">{{ $movie["title"] }}</h2>
+        <p class="movie__text">Original Title: <span class="movie__info">{{ $movie["original_title"] }}</span></p>
+        <p class="movie__text">Nationality: <span class="movie__info">{{ $movie["nationality"] }}</span></p>
+        <p class="movie__text">Vote: <span class="movie__info">{{ $movie["vote"] }}</span></p>
+      </div>
+    @endforeach
+  </main>
 </body>
 </html>
